51bcac8549d0dd1d67c53bd28786dd3bf3a3f39c
kollhdxdlp <eseidel2004@gmail.com>
2023-01-15 11:02:35 +0100

    Tag 2 Morgens

--- test.py
+++ test.py
@@ -1,60 +1,60 @@
-import pygame
-
-pygame.init()
-screen = pygame.display.set_mode((500, 600))
-clock = pygame.time.Clock()
-font = pygame.font.SysFont("Arial", 20)
+import pygame as pg
 
 
-class Button:
-    """Create a button, then blit the surface in the while loop"""
+def rotate(surface, angle, pivot, offset):
+    """Rotate the surface around the pivot point.
 
-    def __init__(self, text,  pos, font, bg="black", feedback=""):
-        self.x, self.y = pos
-        self.font = pygame.font.SysFont("Arial", font)
-        if feedback == "":
-            self.feedback = "text"
-        else:
-            self.feedback = feedback
-        self.change_text(text, bg)
-
-    def change_text(self, text, bg="black"):
-        """Change the text whe you click"""
-        self.text = self.font.render(text, 1, pygame.Color("White"))
-        self.size = self.text.get_size()
-        self.surface = pygame.Surface(self.size)
-        self.surface.fill(bg)
-        self.surface.blit(self.text, (0, 0))
-        self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
-
-    def show(self):
-        screen.blit(button1.surface, (self.x, self.y))
-
-    def click(self, event):
-        x, y = pygame.mouse.get_pos()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if pygame.mouse.get_pressed()[0]:
-                if self.rect.collidepoint(x, y):
-                    self.change_text(self.feedback, bg="red")
+    Args:
+        surface (pygame.Surface): The surface that is to be rotated.
+        angle (float): Rotate by this angle.
+        pivot (tuple, list, pygame.math.Vector2): The pivot point.
+        offset (pygame.math.Vector2): This vector is added to the pivot.
+    """
+    rotated_image = pg.transform.rotozoom(surface, -angle, 1)  # Rotate the image.
+    rotated_offset = offset.rotate(angle)  # Rotate the offset vector.
+    # Add the offset vector to the center/pivot point to shift the rect.
+    rect = rotated_image.get_rect(center=pivot+rotated_offset)
+    return rotated_image, rect  # Return the rotated image and shifted rect.
 
 
-def mainloop():
-    """ The infinite loop where things happen """
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            button1.click(event)
-        button1.show()
-        clock.tick(30)
-        pygame.display.update()
+pg.init()
+screen = pg.display.set_mode((640, 480))
+clock = pg.time.Clock()
+BG_COLOR = pg.Color('gray12')
+# The original image will never be modified.
+IMAGE = pg.Surface((140, 60), pg.SRCALPHA)
+pg.draw.polygon(IMAGE, pg.Color('dodgerblue3'), ((0, 0), (140, 30), (0, 60)))
+# Store the original center position of the surface.
+pivot = [200, 250]
+# This offset vector will be added to the pivot point, so the
+# resulting rect will be blitted at `rect.topleft + offset`.
+offset = pg.math.Vector2(50, 0)
+angle = 0
 
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
 
-button1 = Button(
-    "Click here",
-    (100, 100),
-    font=30,
-    bg="navy",
-    feedback="You clicked me")
+    keys = pg.key.get_pressed()
+    if keys[pg.K_d] or keys[pg.K_RIGHT]:
+        angle += 1
+    elif keys[pg.K_a] or keys[pg.K_LEFT]:
+        angle -= 1
+    if keys[pg.K_f]:
+        pivot[0] += 2
 
-mainloop()
+    # Rotated version of the image and the shifted rect.
+    rotated_image, rect = rotate(IMAGE, angle, pivot, offset)
+
+    # Drawing.
+    screen.fill(BG_COLOR)
+    screen.blit(rotated_image, rect)  # Blit the rotated image.
+    pg.draw.circle(screen, (30, 250, 70), pivot, 3)  # Pivot point.
+    pg.draw.rect(screen, (30, 250, 70), rect, 1)  # The rect.
+    pg.display.set_caption('Angle: {}'.format(angle))
+    pg.display.flip()
+    clock.tick(30)
+
+pg.quit()
